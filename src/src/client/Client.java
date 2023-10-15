5af5629bb82a64f5978c0c652a26c8f3f5b49f09
Braendl <147150093+Braendl@users.noreply.github.com>
2023-10-15 16:01:57 +0200

    Add files via upload

---
+++ src/src/client/Client.java
@@ -0,0 +1,82 @@
+package src.client;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import model.SolarModel;
+
+public class Client implements AutoCloseable {
+    private Connection connection;
+    private Channel channel;
+    private String requestQueueName = "rpc_queue";
+
+    public Client() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+    }
+
+    public static void main(String[] argv) {
+        try (Client client = new Client()) {
+           
+               SolarModel solarModel = new SolarModel();
+               solarModel.setCity("testCity");
+               solarModel.setCountry("testCountry");
+               solarModel.setHouseNumber("3A");
+               solarModel.setPowerSolar(2000);
+               solarModel.setStreet("Lolipopweg");
+           
+                String response = client.call(solarModel);
+                System.out.println(response);
+            
+        } catch (IOException | TimeoutException | InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String call(SolarModel message) throws IOException, InterruptedException, ExecutionException {
+        byte[] sendData;
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream(); 
+                ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+               oos.writeObject(message);
+               sendData = bos.toByteArray();
+           }
+        
+        final String corrId = UUID.randomUUID().toString();
+
+        String replyQueueName = channel.queueDeclare().getQueue();
+        AMQP.BasicProperties props = new AMQP.BasicProperties.Builder().correlationId(corrId).replyTo(replyQueueName)
+                .build();
+
+        channel.basicPublish("", requestQueueName, props, sendData);
+
+        final CompletableFuture<String> response = new CompletableFuture<>();
+
+        String ctag = channel.basicConsume(replyQueueName, true, (consumerTag, delivery) -> {
+            if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response.complete(new String(delivery.getBody(), "UTF-8"));
+            }
+        }, consumerTag -> {
+        });
+
+        String result = response.get();
+        channel.basicCancel(ctag);
+        return result;
+    }
+
+    public void close() throws IOException {
+        connection.close();
+    }
+}
