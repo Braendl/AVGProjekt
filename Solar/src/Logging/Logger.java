f8f333f7696ec50c689e590518123ed598997702
Braendl <147150093+Braendl@users.noreply.github.com>
2023-10-15 16:33:49 +0200

    Update Logger.java

--- Solar/src/Logging/Logger.java
+++ Solar/src/Logging/Logger.java
@@ -6,19 +6,26 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Diese Klasse ermöglicht das Protokollieren von Informationen in eine Textdatei.
+ */
 public class Logger {
     
     public static Date date = new Date();
     
     static SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy_HH-mm-ss");
     
+    /**
+     * Protokolliert den angegebenen Inhalt in eine Textdatei.
+     * @param content Der zu protokollierende Inhalt.
+     */
     public static void log(String content) {
         
         String filePath = "log_" + dateFormat.format(date) + ".txt";
         System.out.println();
         try {
        
-            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true);
 
             writer.write(content);
             writer.newLine();
@@ -30,5 +37,4 @@ public class Logger {
             System.err.println("Fehler beim Logging " + e.getMessage());
         }
     }
-
 }
